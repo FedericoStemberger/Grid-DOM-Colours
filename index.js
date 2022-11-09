4031a700ed693eeda4b9c94c7962ba9391356971
Federico Stemberger <fedestemberger@gmail.com>
2022-11-09 11:11:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,97 @@
+headerVerde = () => {
+    const $header  = document.querySelector(".header");
+    $header.style.setProperty("background-color", "green");
+    console.log("Funciona el boton verde");
+}
+
+headerRojo = () => {
+    const $header  = document.querySelector(".header");
+    $header.style.setProperty("background-color", "red");
+    console.log("Funciona el boton rojo");
+}
+
+headerAmarillo = () => {
+    const $header  = document.querySelector(".header");
+    $header.style.setProperty("background-color", "yellow");
+    console.log("Funciona el boton amarillo");
+}
+
+headerAzul = () => {
+    const $header  = document.querySelector(".header");
+    $header.style.setProperty("background-color", "blue");
+    console.log("Funciona el boton azul");
+}
+
+navVerde = () => {
+    const $nav  = document.querySelector(".nav");
+    $nav.style.setProperty("background-color", "green");
+    console.log("Funciona el boton verde");
+}
+
+navRojo = () => {
+    const $nav  = document.querySelector(".nav");
+    $nav.style.setProperty("background-color", "red");
+    console.log("Funciona el boton rojo");
+}
+
+navAmarillo = () => {
+    const $nav  = document.querySelector(".nav");
+    $nav.style.setProperty("background-color", "yellow");
+    console.log("Funciona el boton amarillo");
+}
+
+navAzul = () => {
+    const $nav  = document.querySelector(".nav");
+    $nav.style.setProperty("background-color", "blue");
+    console.log("Funciona el boton azul");
+}
+
+mainVerde = () => {
+    const $main  = document.querySelector(".main");
+    $main.style.setProperty("background-color", "green");
+    console.log("Funciona el boton verde");
+}
+
+mainRojo = () => {
+    const $main  = document.querySelector(".main");
+    $main.style.setProperty("background-color", "red");
+    console.log("Funciona el boton rojo");
+}
+
+mainAmarillo = () => {
+    const $main  = document.querySelector(".main");
+    $main.style.setProperty("background-color", "yellow");
+    console.log("Funciona el boton amarillo");
+}
+
+mainAzul = () => {
+    const $main  = document.querySelector(".main");
+    $main.style.setProperty("background-color", "blue");
+    console.log("Funciona el boton azul");
+}
+
+footerVerde = () => {
+    const $footer  = document.querySelector(".footer");
+    $footer.style.setProperty("background-color", "green");
+    console.log("Funciona el boton verde");
+}
+
+footerRojo = () => {
+    const $footer  = document.querySelector(".footer");
+    $footer.style.setProperty("background-color", "red");
+    console.log("Funciona el boton rojo");
+}
+
+footerAmarillo = () => {
+    const $footer  = document.querySelector(".footer");
+    $footer.style.setProperty("background-color", "yellow");
+    console.log("Funciona el boton amarillo");
+}
+
+footerAzul = () => {
+    const $footer  = document.querySelector(".footer");
+    $footer.style.setProperty("background-color", "blue");
+    console.log("Funciona el boton azul");
+}
+    
+    
